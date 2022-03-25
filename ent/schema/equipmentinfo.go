2e507a78149aaf22205b327e54b2ec2227bb58de
linqiankai <417350372@qq.com>
2022-03-25 14:30:11 +0800

    schema使用hook存在循环引用问题 把<project>/ent/runtime.go移动到schema下即可

--- ent/schema/equipmentinfo.go
+++ ent/schema/equipmentinfo.go
@@ -1,11 +1,15 @@
 package schema
 
 import (
+	"context"
+
 	"entgo.io/ent"
 	"entgo.io/ent/dialect/entsql"
 	"entgo.io/ent/schema"
 	"entgo.io/ent/schema/edge"
 	"entgo.io/ent/schema/field"
+	gen "github.com/Kotodian/ent-practice/ent"
+	"github.com/Kotodian/ent-practice/ent/hook"
 	"github.com/Kotodian/gokit/datasource"
 	"github.com/Kotodian/gokit/id"
 )
@@ -13,6 +17,17 @@ import (
 // EquipmentInfo holds the schema definition for the EquipmentInfo entity.
 type EquipmentInfo struct {
 	ent.Schema
+}
+
+func (EquipmentInfo) Hooks() []ent.Hook {
+	return []ent.Hook{
+		hook.On(func(m ent.Mutator) ent.Mutator {
+			return hook.EquipmentInfoFunc(func(ctx context.Context, m *gen.EquipmentInfoMutation) (ent.Value, error) {
+				
+				return nil, nil
+			})
+		}, ent.OpUpdate),
+	}
 }
 
 func (EquipmentInfo) Mixins() []ent.Mixin {
