e24ced78739c78a12e5493aab27afb93bf1df11b
linqiankai <417350372@qq.com>
2022-10-21 15:06:02 +0800

    alarm测试

--- enttest/alarm_test.go
+++ enttest/alarm_test.go
@@ -4,17 +4,20 @@ import (
 	"context"
 	"testing"
 
+	"github.com/Kotodian/cwmodel"
+	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateAlarm(t *testing.T) {
-	cli := Open(t, "mysql", dsn)
-	cli = cli.Debug()
+	cli := Open(t, "mysql", dsn, WithOptions(cwmodel.Debug()))
 	defer cli.Close()
 	ctx := context.TODO()
+
 	equip, err := cli.Equipment.Get(ctx, 244667116421190)
 	assert.Nil(t, err)
 	assert.NotNil(t, equip)
+
 	err = cli.EquipmentAlarm.Create().
 		SetDtcCode(16715009).
 		SetCount(0).
@@ -22,4 +25,39 @@ func TestCreateAlarm(t *testing.T) {
 		SetRemoteAddress("127.0.0.1").
 		Exec(ctx)
 	assert.Nil(t, err)
+}
+
+func TestQueryAlarms(t *testing.T) {
+	cli := Open(t, "mysql", dsn, WithOptions(cwmodel.Debug()))
+	defer cli.Close()
+	ctx := context.TODO()
+
+	equip, err := cli.Equipment.Get(ctx, 244667116421190)
+	assert.Nil(t, err)
+	assert.NotNil(t, equip)
+
+	alarms, err := equip.QueryEquipmentAlarm().Unique(false).All(ctx)
+	assert.Nil(t, err)
+	assert.NotNil(t, alarms)
+	t.Log(len(alarms))
+}
+
+func TestUpdateAlarms(t *testing.T) {
+	cli := Open(t, "mysql", dsn, WithOptions(cwmodel.Debug()))
+	defer cli.Close()
+	ctx := context.TODO()
+
+	equip, err := cli.Equipment.Get(ctx, 244667116421190)
+	assert.Nil(t, err)
+	assert.NotNil(t, equip)
+
+	alarms, err := equip.QueryEquipmentAlarm().Unique(false).All(ctx)
+	assert.Nil(t, err)
+	assert.NotNil(t, alarms)
+
+	lo.ForEach(alarms, func(alarm *cwmodel.EquipmentAlarm, _ int) {
+		newAlarm, err := alarm.Update().AddCount(1).Save(ctx)
+		assert.Nil(t, err)
+		assert.Equal(t, 1, newAlarm.Count-alarm.Count)
+	})
 }
