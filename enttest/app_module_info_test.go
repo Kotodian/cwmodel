43fdbbe4f9790a7ff8bafea184e0e9949f3777a0
linqiankai <417350372@qq.com>
2022-10-21 15:07:50 +0800

    app加入Unique

--- enttest/app_module_info_test.go
+++ enttest/app_module_info_test.go
@@ -4,17 +4,20 @@ import (
 	"context"
 	"testing"
 
+	"github.com/Kotodian/cwmodel"
 	"github.com/Kotodian/cwmodel/appmoduleinfo"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestQueryAppModuleInfo(t *testing.T) {
-	cli := Open(t, "mysql", dsn)
-	cli = cli.Debug()
+	cli := Open(t, "mysql", dsn, WithOptions(cwmodel.Debug()))
 	defer cli.Close()
 	ctx := context.TODO()
 
-	appModuleInfo, err := cli.AppModuleInfo.Query().Where(appmoduleinfo.NameEQ("jx-coregw")).Only(ctx)
+	appModuleInfo, err := cli.AppModuleInfo.Query().
+		Unique(false).
+		Where(appmoduleinfo.NameEQ("jx-coregw")).
+		Only(ctx)
 	assert.Nil(t, err)
 	assert.NotNil(t, appModuleInfo)
 }
